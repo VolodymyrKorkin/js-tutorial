8e24fd9eb6ff65feef14bcd9e3c2da1ceee869f7
Vladimir Korkin <korkin.dev@gmail.com>
2022-01-28 10:27:37 +0200

    add solution task 2.4 (Variables)

---
+++ js_basics/2.4_Variables.js
@@ -0,0 +1,18 @@
+'use strict'
+// 2.4.1 Работа с переменными
+/* 
+let admin, name;
+name = 'Джон';
+admin = name
+alert (admin); */
+
+// 2.4.2 Придумайте правильные имена
+/*
+let ourPlanetName = 'Земля'
+let currentUserName = 'Джон' */
+
+// 2.4.3 Какие буквы (заглавные или строчные) использовать для имён констант?
+/*
+const BIRTHDAY;
+const age;
+*/
