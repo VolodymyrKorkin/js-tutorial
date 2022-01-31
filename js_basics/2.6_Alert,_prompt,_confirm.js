7949d73c985affd78b51fa22c2c6023f4ba3e61b
Vladimir Korkin <korkin.dev@gmail.com>
2022-01-31 08:35:47 +0200

    Add solution 2.6 Alert,prompt,confirm. Add 2.6_Alert,_prompt,_confirm.js. Modified index.html

---
+++ js_basics/2.6_Alert,_prompt,_confirm.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// let result = prompt('Введите имя', 'Ivan');
+// alert(`Добрый день, ${result}!`)
+
+// let isBoss = confirm('Ты здесь босс?');
+// alert(isBoss)
+
+// Задача 2.6.1 Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+let userName = prompt('Как Ваше имя?', '');
+alert(`Приветствую, ${userName}!`)
