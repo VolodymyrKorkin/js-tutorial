784ea55714220e896a95fb0314a1081ca68fc896
Vladimir Korkin <korkin.dev@gmail.com>
2022-02-08 08:33:01 +0200

    add solution 2.14 (switch construction)

--- js_basics/2.14_ switch_construction.js
+++ js_basics/2.14_ switch_construction.js
@@ -5,11 +5,11 @@
 //     case 'value1':  // if (x === 'value1')
 //       ...
 //       [break]
-  
+// 
 //     case 'value2':  // if (x === 'value2')
 //       ...
 //       [break]
-  
+// 
 //     default:
 //       ...
 //       [break]
@@ -53,13 +53,13 @@
 //   case 4:
 //     alert('Правильно!');
 //     break;
-
+// 
 //   case 3: // (*) группируем оба case
 //   case 5:
 //     alert('Неправильно!');
 //     alert("Может вам посетить урок математики?");
 //     break;
-
+// 
 //   default:
 //     alert('Результат выглядит странновато. Честно.');
 // }
@@ -67,3 +67,68 @@
 // Теперь оба варианта 3 и 5 выводят одно сообщение.
 
 // !!! проверка на равенство всегда строгая === . Тип имеет значение
+
+/* Задание 2.14.1. Напишите "if", аналогичный "switch" 
+Напишите if..else, соответствующий следующему switch:*/
+
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+// 
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+//  
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+//   }
+
+// let browser = prompt('Введите Ваш браузер', '');
+//
+// if (browser == 'Edge') alert( "You've got the Edge!" );
+//
+// else if (browser == 'Chrome' 
+//   || browser == 'Firefox' 
+//   || browser == 'Safari' 
+//   || browser == 'Opera') {
+//   alert( 'Okay we support these browsers too' );
+// }
+// else alert( 'We hope that this page looks ok!' );
+
+/* Задание 2.14.2. Переписать условия "if" на "switch"
+Перепишите код с использованием одной конструкции switch: */
+
+// const number = +prompt('Введите число между 0 и 3', '');
+// 
+// if (number === 0) {
+//   alert('Вы ввели число 0');
+// }
+// 
+// if (number === 1) {
+//   alert('Вы ввели число 1');
+// }
+// 
+// if (number === 2 || number === 3) {
+//   alert('Вы ввели число 2, а может и 3');
+// }
+
+// const number = +prompt('Введите число между 0 и 3', '');
+// 
+// switch (number) {
+//   case 0:
+//     alert('Вы ввели число 0');
+//   break;
+// 
+//   case 1:
+//     alert('Вы ввели число 1');
+//   break;
+// 
+//   case 2:
+//   case 3:
+//     alert('Вы ввели число 2, а может и 3');
+//   break;
+// }
