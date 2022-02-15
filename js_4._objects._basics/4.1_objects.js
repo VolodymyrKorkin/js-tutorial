34745427be00c09a1bcaa015ac1b73766e024975
Vladimir Korkin <korkin.dev@gmail.com>
2022-02-15 08:52:20 +0200

    add solution 4.1 task 5

--- js_4._objects._basics/4.1_objects.js
+++ js_4._objects._basics/4.1_objects.js
@@ -358,3 +358,34 @@ let salaries = {
 // }
 // 
 // alert(sum); // 390
+
+/* 4.1.5 Задача. Умножаем все числовые свойства на 2
+Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+Например:
+// до вызова функции
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// после вызова функции
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+}; 
+Обратите внимание, что multiplyNumeric не нужно ничего возвращать. 
+Следует напрямую изменять объект.
+P.S. Используйте typeof для проверки, что значение свойства числовое. */
+
+// function multiplyNumeric(menu) {
+//   for (let value in menu) {
+//     if ( typeof( menu[value] ) == 'number' ) {
+//       menu[value] *= 2;
+//     }
+//   }
+// }
+
