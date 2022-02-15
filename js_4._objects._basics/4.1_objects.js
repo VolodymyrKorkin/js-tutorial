1d7343e3ee55c852584e1e86db558f7ef094a130
Vladimir Korkin <korkin.dev@gmail.com>
2022-02-15 08:32:12 +0200

    add solution 4.1 task 2

--- js_4._objects._basics/4.1_objects.js
+++ js_4._objects._basics/4.1_objects.js
@@ -283,7 +283,7 @@
 // Error для хранения информации об ошибке.
 // … и так далее.
 
-/* 4.1 Задача. Привет, object
+/* 4.1.1 Задача. Привет, object
 Напишите код, выполнив задание из каждого пункта отдельной строкой:
 
 Создайте пустой объект user.
@@ -292,13 +292,34 @@
 Измените значение свойства name на Pete.
 Удалите свойство name из объекта. */
 
-let user = {};
+// let user = {};
 
-user.name = 'John';
-user.surname = 'Smith';
-user.name = 'Pete';
-delete user.name;
+// user.name = 'John';
+// user.surname = 'Smith';
+// user.name = 'Pete';
+// delete user.name;
 
+/* 4.1.2 Задача. Проверка на пустоту.
+Напишите функцию isEmpty(obj), которая возвращает true, 
+если у объекта нет свойств, иначе false.
+Должно работать так:
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); // false */
 
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     // если тело цикла начнет выполняться - значит в объекте есть свойства
+//     return false
+//   }
+//   return true
+// }
+// 
+// let obj = {};
+// 
+// obj = prompt('На какое время заводить будильник?', '');
+// 
+// alert(isEmpty(obj));
 
 
