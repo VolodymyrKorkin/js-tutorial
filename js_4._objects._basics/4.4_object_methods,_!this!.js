1855a4126084b194ebfce1fe2ead57538323e088
Vladimir Korkin <korkin.dev@gmail.com>
2022-02-18 09:35:02 +0200

    add solution  4.4 (object methods) task 5

--- js_4._objects._basics/4.4_object_methods,_!this!.js
+++ js_4._objects._basics/4.4_object_methods,_!this!.js
@@ -321,3 +321,45 @@
 // alert( calculator.sum() );
 // alert( calculator.mul() );
 
+/* 4.4.5 Задача. Цепь вызовов
+Это ladder (лестница) – объект, который позволяет подниматься вверх и спускаться:
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function() { // показывает текущую ступеньку
+    alert( this.step );
+  }
+};
+
+Теперь, если нам нужно сделать несколько последовательных вызовов, мы можем выполнить это так:
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+
+Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
+
+ladder.up().up().down().showStep(); // 1 */
+
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//     return this;
+//   },
+//   down() {
+//     this.step--;
+//     return this;
+//   },
+//   showStep() { // показывает текущую ступеньку
+//     return alert( this.step );
+//   }
+// };
+// 
+// ladder.up().up().down().showStep(); // 1
