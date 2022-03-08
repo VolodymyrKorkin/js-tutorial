7c3f8048805c320d3ff0c1163f9ff097b6539db9
Vladimir Korkin <korkin.dev@gmail.com>
2022-03-08 15:14:50 +0100

    add theory 4.6 optional string (question_mark_._)

--- js_4._objects._basics/4.5_constructors,_creating_objects_via_new.js
+++ js_4._objects._basics/4.5_constructors,_creating_objects_via_new.js
@@ -250,17 +250,17 @@ accumulator.read(); // прибавит ввод prompt к текущему зн
 alert(accumulator.value); // выведет сумму этих значений
 */
 
-function Accumulator(startingValue) {
-  this.value = startingValue;
+// function Accumulator(startingValue) {
+//   this.value = startingValue;
 
-  this.read = function() {
-    this.value += +prompt('Сколько нужно добавить?', '0');
-  };
-}
+//   this.read = function() {
+//     this.value += +prompt('Сколько нужно добавить?', '0');
+//   };
+// }
 
-let accumulator = new Accumulator(1); // начальное значение 1
+// let accumulator = new Accumulator(1); // начальное значение 1
 
-accumulator.read(); // прибавит ввод prompt к текущему значению
-accumulator.read(); // прибавит ввод prompt к текущему значению
+// accumulator.read(); // прибавит ввод prompt к текущему значению
+// accumulator.read(); // прибавит ввод prompt к текущему значению
 
-alert(accumulator.value); // выведет сумму этих значений
+// alert(accumulator.value); // выведет сумму этих значений
